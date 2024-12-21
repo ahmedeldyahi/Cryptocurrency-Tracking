301a7715b3647c55754b10cc195a2f766decbed1
Ahmed Eldyahi <ahmed.eldyahi@mdi.com.eg>
2024-12-21 14:39:24 +0200

    Create Cryptocurrency Tracking

---
+++ Cryptocurrency Tracking/Cryptocurrency Tracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cryptocurrency Tracking
+//
+//  Created by Ahmed Eldyahi on 21/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cryptocurrency Tracking/Cryptocurrency Tracking/Cryptocurrency_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryptocurrency_TrackingApp.swift
+//  Cryptocurrency Tracking
+//
+//  Created by Ahmed Eldyahi on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cryptocurrency_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
